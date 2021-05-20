e3ec4abd12fa401a6ce5147a5166b5b24301301a
Cormac Mattimoe <cormacmattimoe98@gmail.com>
2021-05-20 22:19:30 +0100

    Updated classes and added more tests

---
+++ Student Grading System/.history/src/Entities/StudentGrade_20210520164138.java
@@ -0,0 +1,76 @@
+package Entities;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+// Sules 
+// related to them
+
+public class StudentGrade {
+
+    private String studentName;
+    private int grade;
+    ArrayList<Criterion> criteria = new ArrayList<Criterion>();
+    HashMap<String, Integer> listOfAllMarks = new HashMap<String, Integer>();
+
+    
+
+    public StudentGrade() {
+    }
+
+
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+
+
+    public int getGrade() {
+        return grade;
+    }
+
+
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+    //Returns a score of a criterion
+	public int getScore(String criterion) {
+		return listOfAllMarks.get(criterion);
+	}
+
+    //ADDS SCORE TO THE SPECIFIED CRITERION
+	public void addMark(String criterion, int score) {
+		if(score<6 && score >0) {
+		listOfAllMarks.put(criterion,score);
+		}else {
+			throw new IllegalArgumentException("The Score must be );
+		}
+	}
+
+
+	
+	//This is returning criterions marks
+	public HashMap<String, Integer> getCriterionMarks(){
+		return listOfAllMarks;
+	}
+
+
+
+
+    @Override
+    public String toString() {
+        return 
+        "Student{" +
+                "Student Name ='" + studentName + '\'' +
+                ", Student Grade =" + grade +
+                '}';
+    }
+}
